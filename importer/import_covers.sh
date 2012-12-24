14358b3595191583566f415af2c51f2488da62f9
Martin Dietze <mdietze@gmail.com>
2012-12-24 10:58:40 +0100

    Added more import scripts and some first steps documentation.

---
+++ importer/import_covers.sh
@@ -0,0 +1,1 @@
+find ../webapp/images/covers/ -type f | grep -v thumb | ./serializationBasedImporter 
---
+++ importer/import_dehdabehs.sh
@@ -0,0 +1,1 @@
+find ../webapp/images/dehdabehs/ -type f | grep -v thumb | ./serializationBasedImporter 
